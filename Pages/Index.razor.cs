ad63c287b9c95db23d827fdc5905d5bd884ee45d
André Geuze <andregeuze@gmail.com>
2023-01-27 13:53:00 +0100

    Add markdown rendering

---
+++ Pages/Index.razor.cs
@@ -0,0 +1,17 @@
+namespace wasm_blog_engine.Pages;
+
+public partial class Index
+{
+    string markdownContent = "";
+
+    protected override async Task OnInitializedAsync()
+    {
+        // Get raw markdown from a file
+        var client = new HttpClient();
+
+        var rawMarkdown = await client.GetStringAsync("https://localhost:5001/blogs/helloworld.md");
+        if (rawMarkdown == null) { return; }
+
+        markdownContent = rawMarkdown;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using wasm_blog_engine;
 using MudBlazor.Services;
+using MudBlazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+builder.Services.AddMudMarkdownServices();
 
 await builder.Build().RunAsync();
