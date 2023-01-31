2f9f54c477dec7b373e1ba2f0e4dceb2b224b8da
André Geuze <andregeuze@gmail.com>
2023-01-31 20:29:44 +0100

    RESTYRUUU with TailwindCSS

--- Program.cs
+++ Program.cs
@@ -1,15 +1,11 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using wasm_blog_engine;
-using MudBlazor.Services;
-using MudBlazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddMudServices();
-builder.Services.AddMudMarkdownServices();
 
 await builder.Build().RunAsync();
